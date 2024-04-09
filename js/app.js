928dbced73028f52e9bc734c368948a38dd62742
Alice Levchenkova <alice.treecat@gmail.com>
2024-04-09 12:20:53 +0300

    js cards

--- js/app.js
+++ js/app.js
@@ -3,11 +3,16 @@ let lifes = document.getElementById("lifes");
 
 let counterwin = 0;
 
-function generate () {
+function generate (winItem, winId) {
     let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let minus = 5;
     let plus = 2;
-    let win = 2;
+    let win = 2 - winItem;
+    if(winId){
+        let idx = arr.indexOf(winId);
+        arr.splice(idx, 1)
+    }
+
     for (let i = 0; i < minus; i++) {
         let k = Math.floor(Math.random()*(arr.length-1));
         document.getElementById(arr[k]).setAttribute('data-type', '-');
@@ -25,8 +30,9 @@ function generate () {
         document.getElementById(arr[k]).setAttribute('data-type', 'win');
         arr.splice(k, 1);
     }
-}
-generate()
+};
+
+generate(0, null)
 
 cards.forEach((item) => item.addEventListener("click", (e)=>{
     item.classList.add('cards_item-active');
@@ -39,18 +45,19 @@ cards.forEach((item) => item.addEventListener("click", (e)=>{
             alert("loose");
             lifes.innerHTML = 3;
         };
-        generate()
+        generate(0, null)
     } else if (type === "+") {
         item.classList.add('cards_item-nonactive');
         lifes.innerHTML++;
-        generate();
+        generate(0, null);
     } else if (type === "win"){
+
         counterwin++
-        item.classList.add('cards_item-nonactive')
+        item.classList.add('cards_item-win')
         if(counterwin === 2){
             alert("WIN")
             lifes.innerHTML = 3
         };
-        generate();
+        generate(counterwin, item.id);
     }
 }))
