91058905f0e49487edd8d6e212dce2c4e6201735
Alice Levchenkova <alice.treecat@gmail.com>
2024-04-09 16:28:29 +0300

    add comments, add cardwin array and logic generate 2 win cards

--- js/app.js
+++ js/app.js
@@ -2,44 +2,46 @@ let cards = document.querySelectorAll("button");
 let lifes = document.getElementById("lifes");
 
 let counterwin = 0;
+let cardwin = []; // будем сюда складывать карточки, которые угадались
 
 function generate (winItem, winId) {
-    let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    let indexCards = [0, 1, 2, 3, 4, 5, 6, 7, 8];
     let minus = 5;
     let plus = 2;
     let win = 2 - winItem;
 
-    if(winId){
-        let idx = arr.indexOf(winId);
-        arr.splice(idx, 1)
-    }
+    if(cardwin.length){
+        cardwin.forEach(item => {
+            let idx = indexCards.indexOf(item);
+            indexCards.splice(idx, 1)
+        })
+    } // вырезаем карточки из генерации, если они были выиграшные из массива
 
     for (let i = 0; i < minus; i++) {
-        let k = Math.floor(Math.random()*(arr.length));
-        document.getElementById(arr[k]).setAttribute('data-type', '-');
-        arr.splice(k, 1);
+        let k = Math.floor(Math.random()*(indexCards.length));
+        document.getElementById(indexCards[k]).setAttribute('data-type', '-');
+        indexCards.splice(k, 1);
 
-    }
+    } // генерация карт, в которых теряем жизнь
     for (let i = 0; i < plus; i++) {
-        let k = Math.floor(Math.random()*(arr.length));
-        document.getElementById(arr[k]).setAttribute('data-type', '+');
-        arr.splice(k, 1);
+        let k = Math.floor(Math.random()*(indexCards.length));
+        document.getElementById(indexCards[k]).setAttribute('data-type', '+');
+        indexCards.splice(k, 1);
 
-    }
+    } // генерация карт, в которых добавляем жизнь
+    if(cardwin.length === 2) return
     for (let i = 0; i < win; i++) {
-        let k = Math.floor(Math.random()*(arr.length));
-        document.getElementById(arr[k]).setAttribute('data-type', 'win');
-        arr.splice(k, 1);
-    }
+        document.getElementById(indexCards[i]).setAttribute('data-type', 'win');
+    } // генерация карт, которые нужно угадать
 };
 
-generate(0, null)
+generate(0, null);
 
 cards.forEach((item) => item.addEventListener("click", (e)=>{
     item.classList.add('cards_item-active');
     let type = item.getAttribute("data-type");
     let id = item.id;
-    console.log(type);
+    if(cardwin.includes(id)) return; // ничего не делать, если карточку уже угадали
     if(type === "-"){
         item.classList.add('cards_item-minus')
         lifes.innerHTML--;
@@ -62,12 +64,12 @@ cards.forEach((item) => item.addEventListener("click", (e)=>{
         lifes.innerHTML++;
         generate(0, null);
     } else if (type === "win"){
-
         counterwin++
+        cardwin.push(id)
         item.classList.add('cards_item-win')
-        if(counterwin === 2){
+        if(counterwin === 100){
             alert("WIN")
-            lifes.innerHTML = 3
+            lifes.innerHTML = 3;
         };
         generate(counterwin, item.id);
     }
